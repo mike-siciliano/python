afac03107d1bdbd85a57b1c1115c9152e7b8489c
Mike Siciliano <msiciliano@exagrid.com>
2023-07-10 15:50:43 -0400

    Find the median of 2 sorted arrays

---
+++ array_median.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from typing import List
+
+'''
+    LeetCode #4
+    https://leetcode.com/problems/median-of-two-sorted-arrays/
+'''
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+      
+        nums1.extend(nums2)
+        nums1.sort()
+        array_length = len(nums1)
+      
+        if array_length % 2 == 0:
+            # array is even 
+            midpoint = array_length // 2 # floor division
+            value = (nums1[midpoint - 1] + nums1[midpoint]) / 2 # average of values
+            return value
+        else: 
+            # array is odd 
+            value =  nums1[(array_length // 2)] # floor division 
+            return value
+
+# Driver code
+if __name__ == '__main__':
+    nums1 = [1,4,7,10,12]
+    nums2 = [2,3,6,15]
+
+    soulution = Solution()
+    print(soulution.findMedianSortedArrays(nums1, nums2))
